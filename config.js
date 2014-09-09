b7c678bfce741895ecac012d1bdc99f48435b2e9
Sajju80 <sajid.qayyum404@gmail.com>
2014-09-09 19:03:46 +0100

    Update config.js
    Improved formatting

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
 	// When running Ghost in the wild, use the production environment
 	// Configure your URL and mail settings here
 	production: {
-		url: 'http://my-ghost-blog.com',
+		url: process.env.BLOG_URL,
 		mail: {},
 		database: {
 			client: 'mysql',
